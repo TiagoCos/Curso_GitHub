68919ba27dcf3f4a570921d0cd072b5d2443aa4a
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-08-14 20:18:54 -0300

    exercicio

---
+++ curso/exercicio.js
@@ -0,0 +1,16 @@
+const person = {
+    name : "John",
+    age: 30
+};
+
+Object.defineProperty(person, "profession", {
+    value: "Engineer",
+    writable: false
+});
+
+person.name = "Jane";
+person.profession = "Doctor";
+
+console.log(person.name)
+console.log(person.profession)
+
